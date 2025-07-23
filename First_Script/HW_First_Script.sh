d0ab577027b135dc07c252efa43e89f44eaf8bd0
Sasha <Filimonov_av@list.ru>
2025-07-23 16:55:06 +0500

    add LOG file, update script (+log, dont work)

--- First_Script/HW_First_Script.sh
+++ First_Script/HW_First_Script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
 # Parser
-date_sytem=$(date)
+date_system=$(date 2>First_Script.log)
 work_dir=$(pwd)
 user_name=$(who)
 OS_info=$(hostnamectl | grep -e "Operating" -e "Kernel")
 
 # Composer
-$date_system > My_File.txt
+echo "$date_system" > My_File.txt
 echo "$work_dir" >> My_File.txt
-echo "$who" >> My_File.txt
+echo "$user_name" >> My_File.txt
 echo "$OS_info" >> My_File.txt
