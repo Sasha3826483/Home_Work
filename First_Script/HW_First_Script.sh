37386d1a239331e417581bbdabfc5a832522d2e2
Sasha <Filimonov_av@list.ru>
2025-07-23 15:54:57 +0500

    update files

--- First_Script/HW_First_Script.sh
+++ First_Script/HW_First_Script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -x
 
+# Parser
 date_sytem=$(date)
 work_dir=$(pwd)
 user_name=$(who)
-#OS_info=$(hostnamectl | grep -e "Operating" -e "Kernel")
+OS_info=$(hostnamectl | grep -e "Operating" -e "Kernel")
 
-echo "$date_system" > My_File.txt
+# Composer
+$date_system > My_File.txt
 echo "$work_dir" >> My_File.txt
 echo "$who" >> My_File.txt
-#echo "$OS_info" >> My_File.txt
+echo "$OS_info" >> My_File.txt
